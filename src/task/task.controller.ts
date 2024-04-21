b5468bb588e7d7cb1c60c9707afede787b6a58e7
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-20 21:19:22 -0600

    se agrego la referencia al servicio, se agrego una funcion que retorna un arreglo del tipo de tareas, se agrego la importacion de la entidad y del emun de la tarea

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
+
+    constructor(private taskService: TaskService){}
     
     @Get()
-    heloworld(){
-        return 'Hello World';
+    getListTask(){
+        return this.taskService.getAllTask();
     }
 }
